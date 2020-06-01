6575360ad70ca879b9b60f05ad38dad7c626e309
hobada96 <hobada96@gmail.com>
2020-06-01 23:45:12 +0900

    this is 0526 lecture

---
+++ 0526/ex_write.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	FILE *fp = fopen("dum","w");
+	
+	if(fp)
+	{
+		fprintf(fp,"Hello\n");
+		fclose(fp);	
+	}
+	else
+		printf("fopen error\n");
+	return 0;
+}
